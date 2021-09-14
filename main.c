937b8da7e029f71220b6ce6f94dde120b9eec652
athomas355 <ashishthomas972@gmail.com>
2021-09-14 16:29:50 -0500

    The first commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "node.h"
+#include "traversals.h"
+#include "buildTree.h"
+
+int main(int argc, char* argv[]) {
+    
+    //process command line arguments and make sure file is readable, error otherwise
+
+    //setup keyboard processing so that below the input method is not relevant
+
+    return 0;
+}
